e3df1806215448c38b54b767165b777a835903f5
viktor <ignatev-viktor@yandex.ru>
2022-12-19 04:07:30 +0800

    правки тестировка

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -2,8 +2,19 @@ const initBurger = () => {
   const siteList = document.querySelector('.main-nav__list--header');
   const button = document.querySelector('.main-nav__toggle');
   const header = document.querySelector('.main-header');
-  const logoIcon = document.querySelector('.logo-link__icon--header');
+  const logoIcon = document.querySelector('.logo-link__icon');
   const overlay = document.querySelector('.overlay');
+
+  header.classList.remove('main-header--no-js');
+
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('overlay--active');
+    header.classList.remove('main-header--opened');
+    button.classList.remove('main-nav__toggle--closed');
+    button.classList.add('main-nav__toggle--opened');
+    siteList.style.display = 'none';
+    logoIcon.style.fill = '$color-alice-blue';
+  });
 
   button.addEventListener('click', () => {
     if (button.classList.contains('main-nav__toggle--closed')) {
@@ -12,14 +23,15 @@ const initBurger = () => {
       button.classList.remove('main-nav__toggle--closed');
       button.classList.add('main-nav__toggle--opened');
       siteList.style.display = 'none';
-      logoIcon.style.fill = '#f9fbfd';
+      logoIcon.style.fill = '$color-alice-blue';
     } else {
       overlay.classList.add('overlay--active');
+      header.classList.remove('main-header--no-js');
       button.classList.remove('main-nav__toggle--opened');
       button.classList.add('main-nav__toggle--closed');
       siteList.style.display = 'block';
-      logoIcon.style.fill = '#011c40';
       header.classList.add('main-header--opened');
+      logoIcon.style.fill = '$color-tangaroa';
     }
   });
 };
