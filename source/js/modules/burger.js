df6b437f9bdd9c1c834301a5aeab33bcbb07fe87
viktor <ignatev-viktor@yandex.ru>
2022-12-26 00:40:13 +0800

    правки тестировка

--- source/js/modules/burger.js
+++ source/js/modules/burger.js
@@ -2,8 +2,9 @@ const initBurger = () => {
   const siteList = document.querySelector('.main-nav__list--header');
   const button = document.querySelector('.main-nav__toggle');
   const header = document.querySelector('.main-header');
-  const logoIcon = document.querySelector('.logo-link__icon');
+  const logoIcon = document.querySelector('.logo-link__icon--header');
   const overlay = document.querySelector('.overlay');
+  const mainNavLinks = document.querySelectorAll('.main-nav__link--header');
 
   header.classList.remove('main-header--no-js');
 
@@ -13,7 +14,7 @@ const initBurger = () => {
     button.classList.remove('main-nav__toggle--closed');
     button.classList.add('main-nav__toggle--opened');
     siteList.style.display = 'none';
-    logoIcon.style.fill = '$color-alice-blue';
+    logoIcon.style.fill = '#f9fbfd';
   });
 
   button.addEventListener('click', () => {
@@ -23,7 +24,9 @@ const initBurger = () => {
       button.classList.remove('main-nav__toggle--closed');
       button.classList.add('main-nav__toggle--opened');
       siteList.style.display = 'none';
-      logoIcon.style.fill = '$color-alice-blue';
+      document.body.style.overflow = '';
+      logoIcon.style.fill = '#f9fbfd';
+
     } else {
       overlay.classList.add('overlay--active');
       header.classList.remove('main-header--no-js');
@@ -31,8 +34,21 @@ const initBurger = () => {
       button.classList.add('main-nav__toggle--closed');
       siteList.style.display = 'block';
       header.classList.add('main-header--opened');
-      logoIcon.style.fill = '$color-tangaroa';
+      document.body.style.overflow = 'hidden';
+      logoIcon.style.fill = '#011c40';
     }
+  });
+
+  mainNavLinks.forEach((e) => {
+    e.addEventListener('click', () => {
+      overlay.classList.remove('overlay--active');
+      header.classList.remove('main-header--opened');
+      button.classList.remove('main-nav__toggle--closed');
+      button.classList.add('main-nav__toggle--opened');
+      siteList.style.display = 'none';
+      document.body.style.overflow = '';
+      logoIcon.style.fill = '#f9fbfd';
+    });
   });
 };
 
